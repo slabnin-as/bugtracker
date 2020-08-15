57c9982701aaf1a778214550ea3d39b8df7bf96b
Anton Slabnin <insk54@gmail.com>
2020-08-16 04:01:20 +0700

    version 0.4

--- src/main/java/db/createDB.sql
+++ src/main/java/db/createDB.sql
@@ -12,11 +12,10 @@ CREATE TABLE public.users
 (
     password character varying(32) COLLATE pg_catalog."default" NOT NULL,
     name character varying(32) COLLATE pg_catalog."default" NOT NULL,
-    login character varying(32) COLLATE pg_catalog."default" NOT NULL,
+    login character varying(32) COLLATE pg_catalog."default" UNIQUE NOT NULL,
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 100000 MINVALUE 100000 MAXVALUE 999999 CACHE 1 ),
     role users_role NOT NULL,
-    CONSTRAINT users_pkey PRIMARY KEY (id),
-    CONSTRAINT login UNIQUE (login)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE public.issues
@@ -33,3 +32,21 @@ CREATE TABLE public.issues
     create_date date NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE public.projects
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1000 MINVALUE 1000 MAXVALUE 9999 CACHE 1 ),
+    title character varying(45) NOT NULL,
+    department character varying(45) NOT NULL,
+    owner_id integer NOT NULL,
+    admin_id integer NOT NULL,
+    create_date date NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE public.projects ADD CONSTRAINT user_id FOREIGN KEY (owner_id) REFERENCES public.users (id)
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION;
+ALTER TABLE public.issues ADD CONSTRAINT project_id FOREIGN KEY (project_id) REFERENCES public.projects (id)
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION;
--- src/main/java/model/Project.java
+++ src/main/java/model/Project.java
@@ -1,7 +1,19 @@
 package model;
 
+import java.time.LocalDate;
+import java.util.Map;
+
 public class Project {
     private Long id;
+    private String title;
+    private String department;
+    private Long owner_id;
+    private Long admin_id;
+
+    private Map<Long,Issue> backlog;
+    private Map<Long,Issue> sprint;
+
+    private LocalDate createDate;
 
     public Long getId() {
         return id;
